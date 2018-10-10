e780c70d97440a280f31a276929f06cc9b97103a
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-10-10 15:52:32 +0000

    change command
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- onos_kidash.sh
+++ onos_kidash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kidash --kibana-url http://0.0.0.0:5601 --elastic_url-enrich http://localhost:9200 --import dashboards/git-dashboard.json
-kidash --kibana-url http://0.0.0.0:5602 --elastic_url-enrich http://localhost:9200 --import dashboards/github-dashboard.json
+kidash --kibana-url http://localhost:5601 --elastic_url-enrich http://localhost:9200 --import dashboards/git-dashboard.json
+kidash --kibana-url http://localhost:5601 --elastic_url-enrich http://localhost:9200 --import dashboards/github-dashboard.json
