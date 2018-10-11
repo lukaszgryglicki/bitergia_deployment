6d15c74591dc2d90a94ef0f859f15176f6b77976
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-10-11 16:51:29 +0000

    Support adding organizations
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- cmd/json2hat/json2hat.go
+++ cmd/json2hat/json2hat.go
@@ -29,6 +29,17 @@ type gitHubUser struct {
 	SexProb     *float64 `json:"sex_prob"`
 }
 
+// affData - holds single affiliation data
+type affData struct {
+	uuid    string
+	company string
+	from    time.Time
+	to      time.Time
+}
+
+// stringSet - set of strings
+type stringSet map[string]struct{}
+
 func fatalOnError(err error) {
 	if err != nil {
 		tm := time.Now()
@@ -48,8 +59,26 @@ func emailDecode(line string) string {
 	return re.ReplaceAllString(line, `$1@$2`)
 }
 
+func timeParseAny(dtStr string) time.Time {
+	formats := []string{
+		"2006-01-02 15:04:05",
+		"2006-01-02 15:04",
+		"2006-01-02 15",
+		"2006-01-02",
+		"2006-01",
+		"2006",
+	}
+	for _, format := range formats {
+		t, e := time.Parse(format, dtStr)
+		if e == nil {
+			return t
+		}
+	}
+	fatalf("Error:\nCannot parse date: '%v'\n", dtStr)
+	return time.Now()
+}
+
 func updateProfile(db *sql.DB, uuid string, user *gitHubUser) {
-	query := ""
 	var cols []string
 	var args []interface{}
 	if user.Sex != nil && (*user.Sex == "m" || *user.Sex == "f") {
@@ -57,11 +86,42 @@ func updateProfile(db *sql.DB, uuid string, user *gitHubUser) {
 		if *user.Sex == "f" {
 			gender = "female"
 		}
-		cols = append(cols, "gender")
+		cols = append(cols, "gender = ?")
 		args = append(args, gender)
 	}
-	_, err := db.Exec(query, args...)
+	if user.SexProb != nil {
+		cols = append(cols, "gender_acc = ?")
+		args = append(args, int(*user.SexProb*100.0))
+	}
+	if user.CountryID != nil {
+		cols = append(cols, "country_code = ?")
+		args = append(args, strings.ToUpper(*user.CountryID))
+	}
+	if len(cols) > 0 {
+		query := strings.Join(cols, ", ")
+		query = "update profiles set " + query + " where uuid = ?"
+		args = append(args, uuid)
+		// fmt.Printf("%s\n", query)
+		_, err := db.Exec(query, args...)
+		fatalOnError(err)
+	}
+}
+
+func addOrganization(db *sql.DB, company string) int {
+	_, err := db.Exec("insert into organizations(name) values(?)", company)
 	fatalOnError(err)
+	rows, err := db.Query("select id from organizations where name = ?", company)
+	fatalOnError(err)
+	var id int
+	for rows.Next() {
+		fatalOnError(rows.Scan(&id))
+	}
+	fatalOnError(rows.Err())
+	fatalOnError(rows.Close())
+	return id
+}
+
+func addEnrollment(db *sql.DB, uuid string, companyID int, dtFrom, dtTo time.Time) {
 }
 
 func importAffs(db *sql.DB, users *gitHubUsers) {
@@ -108,12 +168,18 @@ func importAffs(db *sql.DB, users *gitHubUsers) {
 	fatalOnError(rows.Close())
 
 	// Process all JSON entries
+	defaultStartDate := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	defaultEndDate := time.Date(2099, 1, 1, 0, 0, 0, 0, time.UTC)
+	companies := make(stringSet)
+	var affList []affData
 	hits := 0
+	allAffs := 0
 	for _, user := range *users {
 		// Email decode ! --> @
 		user.Email = strings.ToLower(emailDecode(user.Email))
 		email := user.Email
 		login := user.Login
+		// Update profiles
 		uuids := make(map[string]struct{})
 		uuid, ok := email2uuid[email]
 		if ok {
@@ -128,9 +194,65 @@ func importAffs(db *sql.DB, users *gitHubUsers) {
 				updateProfile(db, uuid, &user)
 			}
 			hits++
+			// Affiliations
+			affs := user.Affiliation
+			if affs == "NotFound" || affs == "(Unknown)" || affs == "?" || affs == "" {
+				continue
+			}
+			affsAry := strings.Split(affs, ", ")
+			prevDate := defaultStartDate
+			for _, aff := range affsAry {
+				var dtFrom, dtTo time.Time
+				ary := strings.Split(aff, " < ")
+				company := strings.TrimSpace(ary[0])
+				if len(ary) > 1 {
+					// "company < date" form
+					dtFrom = prevDate
+					dtTo = timeParseAny(ary[1])
+				} else {
+					// "company" form
+					dtFrom = prevDate
+					dtTo = defaultEndDate
+				}
+				if company == "" {
+					continue
+				}
+				companies[company] = struct{}{}
+				for uuid := range uuids {
+					affList = append(affList, affData{uuid: uuid, company: company, from: dtFrom, to: dtTo})
+					allAffs++
+				}
+				prevDate = dtTo
+			}
 		}
 	}
-	fmt.Printf("Hits: %d\n", hits)
+	// fmt.Printf("affList: %+v\ncompanies: %+v\n", affList, companies)
+
+	// Add companies
+	for company := range companies {
+		if company == "" {
+			continue
+		}
+		id, ok := oname2id[company]
+		if !ok {
+			id = addOrganization(db, company)
+			oname2id[company] = id
+		}
+	}
+
+	// Add enrollments
+	for _, aff := range affList {
+		uuid := aff.uuid
+		if aff.company == "" {
+			continue
+		}
+		companyID, ok := oname2id[aff.company]
+		if !ok {
+			fatalf("company not found: " + aff.company)
+		}
+		addEnrollment(db, uuid, companyID, aff.from, aff.to)
+	}
+	fmt.Printf("Hits: %d, affiliations: %d, companies: %d\n", hits, allAffs, len(companies))
 }
 
 func main() {
