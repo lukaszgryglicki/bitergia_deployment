a6377da2f2534635dbfa9d6555cc09dba069ca25
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-11 13:58:08 +0000

    Fix typo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- onos_kidash.sh
+++ onos_kidash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/git-dashboard.json
 kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/github-dashboard.json
-kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/github-sortinghat.json
+kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/git-sortinghat.json
 kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --list
