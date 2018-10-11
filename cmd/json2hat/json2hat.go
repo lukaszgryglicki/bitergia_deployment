2c2bea26b7fcc73f9e5e25401dd6a5a0fd798ca0
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-10-11 14:47:20 +0000

    Import affiliations from devstats JSON - skeleton
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- cmd/json2hat/json2hat.go
+++ cmd/json2hat/json2hat.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime/debug"
+	"time"
 )
 
+// gitHubUsers - list of GitHub user data from cncf/gitdm.
+type gitHubUsers []gitHubUser
+
+// gitHubUser - single GitHug user entry from cncf/gitdm `github_users.json` JSON.
+type gitHubUser struct {
+	Login       string   `json:"login"`
+	Email       string   `json:"email"`
+	Affiliation string   `json:"affiliation"`
+	Name        string   `json:"name"`
+	CountryID   *string  `json:"country_id"`
+	Sex         *string  `json:"sex"`
+	Tz          *string  `json:"tz"`
+	SexProb     *float64 `json:"sex_prob"`
+}
+
+func fatalOnError(err error) {
+	if err != nil {
+		tm := time.Now()
+		fmt.Printf("Error(time=%+v):\nError: '%s'\nStacktrace:\n%s\n", tm, err.Error(), string(debug.Stack()))
+		fmt.Fprintf(os.Stderr, "Error(time=%+v):\nError: '%s'\nStacktrace:\n", tm, err.Error())
+		panic("stacktrace")
+	}
+}
+
+func fatalf(f string, a ...interface{}) {
+	fatalOnError(fmt.Errorf(f, a...))
+}
+
 func main() {
-	fmt.Printf("hello\n")
+	// Parse github_users.json
+	var users gitHubUsers
+	data, err := ioutil.ReadFile("./github_users.json")
+	if err != nil {
+		fatalOnError(err)
+	}
+	fatalOnError(json.Unmarshal(data, &users))
+	fmt.Printf("ok\n")
 }
