d554cdb8705d8086a19e2afbf9a53be329a297e1
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-10-12 07:58:34 +0000

    Mordred works with Arthur
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ mordred.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sirmordred -c mordred.cfg &
--- onos_arthurd.sh
+++ onos_arthurd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # arthurd -h 127.0.0.1 -d http://127.0.0.1:6379 --es-index http://localhost:9200/items --log-path logs_arthud --no-archive
-arthurd -h 127.0.0.1 -d http://127.0.0.1:6379 --es-index http://localhost:9200/items --log-path logs_arthurd
+arthurd -h 127.0.0.1 -d http://127.0.0.1:6379 --es-index http://localhost:9200/items --log-path arthurd_logs
