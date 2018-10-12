5c11886ac8cf9bebf6e214f9172d9e33488fed8f
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-10-12 09:11:55 +0000

    Dashboards for K8s repo
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ k8s_kidash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/git-k8s.json
+kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --import dashboards/github-k8s.json
+kidash --kibana-url http://127.0.0.1:5601 -e http://147.75.97.234:9200 --elastic_url-enrich http://147.75.97.234:9200 --list
