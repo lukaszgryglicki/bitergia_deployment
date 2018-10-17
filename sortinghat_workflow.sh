18d8c67bab77349ee727f9e513934da8e4f3d974
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-17 11:28:00 +0000

    Fix execute paths
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sortinghat_workflow.sh
+++ sortinghat_workflow.sh
@@ -12,6 +12,6 @@ fi
 # sortinghat orgs -a Example
 # sortinghat enroll --from 2014-06-01 --to 2015-09-01 a9b403e150dd4af8953a52a4bb841051e4b705d9 Example
 # sortinghat enroll --from 2015-09-01 a9b403e150dd4af8953a52a4bb841051e4b705d9 Individual
-unify_sortinghat.sh
-autoprofile_sortinghat.sh
-autogender_sortinghat.sh
+./unify_sortinghat.sh
+./autoprofile_sortinghat.sh
+./autogender_sortinghat.sh
