7c015e6dbd435924f488e15604998664ca30b432
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-18 11:25:38 +0000

    Added .kibana index aliast to allow switchign dashboards too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/create_es_index.sh
+++ es/create_es_index.sh
@@ -4,5 +4,12 @@ then
   echo "$0: please provide index name as an argument"
   exit 1
 fi
+TYPE=items
+base=${1:0:7}
+if [ "${base}" = '.kibana' ]
+then
+  TYPE=doc
+fi
 curl -XPUT "localhost:9200/${1}"
-curl -XPOST -H 'Content-Type: application/json' "localhost:9200/${1}/items?refresh=wait_for" -d '{}'
+echo "${1}/${TYPE}"
+curl -XPOST -H 'Content-Type: application/json' "localhost:9200/${1}/${TYPE}?refresh=wait_for" -d '{}'
--- es/switch_all.sh
+++ es/switch_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-INDEXES='git github gerrit slack jira confluence git_raw github_raw gerrit_raw slack_raw jira_raw confluence_raw' ./es/switch_aliases.sh
+INDEXES='git github gerrit slack jira confluence git_raw github_raw gerrit_raw slack_raw jira_raw confluence_raw .kibana' ./es/switch_aliases.sh
