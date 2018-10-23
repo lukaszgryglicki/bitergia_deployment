c162ee1e3431fdcaa7cb7f9f2542ad112b47f482
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-23 10:54:08 +0000

    Add testing lucene tool
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ es/query_es_index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -z "${ES_URL}" ]
+then
+  ES_URL="localhost:9200"
+fi
+if [ -z "$1" ]
+then
+  echo "$0: please provide index name as a first argument"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: please provide type name as a second argument"
+  exit 2
+fi
+if [ -z "$3" ]
+then
+  echo "$0: please provide search query as a third argument:"
+  echo '{"query":{"query_string":{"query":"type:svars AND vname:full_name"}}}'
+  exit 3
+fi
+curl -XPOST -H 'Content-Type: application/json' "${ES_URL}/${1}/${2}/_search?pretty" -d "$3"
