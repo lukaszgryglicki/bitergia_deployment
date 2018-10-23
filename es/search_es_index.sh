8a1a94fbd41f3a3fdb431fd313dee7028f471998
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-23 09:14:54 +0000

    few more examples
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/search_es_index.sh
+++ es/search_es_index.sh
@@ -19,11 +19,17 @@ then
   echo '{"query":{"match":{"type":"tquick_ranges"}}}'
   echo '{"query":{"match":{"type":"sannotations"}}}'
   echo '{"query":{"match":{"type":"tcountries"}}}'
+  echo '{"query":{"match":{"type":"trepo_groups"}}}'
   echo '{"query":{"match":{"type":"sevents_h"}}}'
   echo '{"query":{"match":{"type":"sact"}}}'
   echo '{"query":{"match":{"type":"sactivity_repo_groups"}}}'
   echo '{"query":{"match":{"period":"q"}}}'
   echo '{"query":{"match":{"type":"svars"}}}'
+  echo '{"query":{"bool":{"must":[{"match":{"type":"svars"}},{"match":{"vname":"full_name"}}]}}}'
+  echo '{"query":{"bool":{"must":[{"match":{"type":"trepo_groups"}},{"match":{"repo_group_name":"cncf/devstats"}}]}}}'
+  echo '{"query":{"bool":{"must":[{"match":{"type":"sannotations"}},{"match":{"title":"1.0"}}]}}}'
+  echo '{"query":{"bool":{"must":[{"match":{"type":"tquick_ranges"}},{"match":{"quick_ranges_suffix":"y10"}}]}}}'
+  echo '{"query":{"bool":{"must":[{"match":{"type":"tquick_ranges"}},{"match":{"quick_ranges_suffix":"a_10_n"}}]}}}'
   exit 3
 fi
 curl -XPOST -H 'Content-Type: application/json' "${ES_URL}/${1}/${2}/_search?pretty" -d "$3"
