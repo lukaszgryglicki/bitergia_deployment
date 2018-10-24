823b4e9f351890e34bd53801f21376fc2e4b9e13
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-24 10:54:58 +0000

    watchers example
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- es/query_es_index.sh
+++ es/query_es_index.sh
@@ -22,6 +22,7 @@ then
   echo '{"query":{"query_string":{"query":"type:itvars AND iname:vvalue AND tag_time:\"2014-01-01 13:00:00\""}}}'
   echo '{"query":{"query_string":{"query":"type:sactivity_repo_groups AND data.ivalue:0.14"}}}'
   echo '{"query":{"query_string":{"query":"type:isactivity_repo_groups AND period:y AND name:(\"cncf\/devstats\" OR \"cncf\/gitdm\")"}}}'
+  echo '{"query":{"query_string":{"query":"type:iswatchers AND period:d AND series:watchcncflandscapewatch"}}}'
   exit 3
 fi
 curl -XPOST -H 'Content-Type: application/json' "${ES_URL}/${1}/${2}/_search?pretty" -d "$3"
